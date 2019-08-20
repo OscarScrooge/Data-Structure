e18e6c43e11201113349d30a59df344f7e2999af
oscar-mandros <mscb.osanchez@gmail.com>
2019-08-20 10:33:22 -0500

    iniciando la lista simple

---
+++ listaSimple_1.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct Node{
+
+   float value;
+   struct Node *next;
+
+}Node;
+
+Node *newNode(float value, Node *next);
+
+void printList(Node *head);
+
+
+
+void main(){
+
+    int yes=1;
+    float value =0.0;
+    Node *head = NULL;
+   
+    while(yes == 1){
+
+      printf("\n\nAgregue un numero a la lista: ");
+      scanf("%f",&value);
+
+      if(head==NULL){
+         head = newNode(value,NULL);      	
+      }else{
+         head = newNode(value,head);
+      }
+      system("clear");
+      printf("\n\nDesea agregar otro numero?: si = 1, no = 0\n");
+      printf(": ");
+      scanf("%d",&yes);
+      system("clear");
+
+    }
+
+    printList(head);
+
+}
+
+Node *newNode(float value,Node *next){
+   
+    Node *ptr;
+    ptr = (Node*) malloc(sizeof(Node));
+    ptr -> value = value;
+    ptr -> next = next;
+
+    return ptr;
+}
+
+void printList(Node *head){
+
+     printf("\n\n");
+     while(head != NULL){
+           printf("%4f\n", head -> value);
+           head = head -> next;
+
+     }
+
+}
+
+
+
+
+
+
+
